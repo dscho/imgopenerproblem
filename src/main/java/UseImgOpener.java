63c0fa7332ba4f9e104052b1b7e39b8e95d56cb3
Tobias Pietzsch <tobias.pietzsch@gmail.com>
2014-04-11 02:17:35 +0200

    example project for testing scifio ImgOpener

---
+++ src/main/java/UseImgOpener.java
@@ -0,0 +1,18 @@
+import io.scif.img.ImgOpener;
+import io.scif.img.SCIFIOImgPlus;
+
+public class UseImgOpener
+{
+	public static void main( final String[] args ) throws Exception
+	{
+		useImgOpener( "/Users/pietzsch/Downloads/HisYFP-SPIM/spim_TL18_Angle0.lsm" );
+	}
+
+	public static void useImgOpener( final String fn ) throws Exception
+	{
+		final ImgOpener o = new ImgOpener();
+		final SCIFIOImgPlus img = o.openImg( fn );
+		img.cursor().next();
+		System.out.println( "done" );
+	}
+}
